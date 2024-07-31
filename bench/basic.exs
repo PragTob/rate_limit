7a15cf3b5676eb42ccbc31423488c1df93c6c5be
Tobias Pfeiffer <pragtob@gmail.com>
2024-07-31 10:42:36 +0200

    Change up Benchmark
    * Remove control group and custom implementations, otherwise memory
    is too easy to blow up during the run
    * fiddled with times so that we start with an ETS that is already
    more full
    * generate the random key number inside of before_each so that the
    generation does not affect the measured time
    * made the range of values generated adjustable, as that can
    possibly have a huge impact on the results
    * also print extended statistics

--- bench/basic.exs
+++ bench/basic.exs
@@ -1,43 +1,42 @@
+# MIX_ENV=bench LIMIT=1 SCALE=5000 RANGE=10000 PARALLEL=500 mix run bench/basic.exs
 profile? = !!System.get_env("PROFILE")
 parallel = String.to_integer(System.get_env("PARALLEL", "1"))
 limit = String.to_integer(System.get_env("LIMIT", "1000000"))
 scale = String.to_integer(System.get_env("SCALE", "60000"))
+range = String.to_integer(System.get_env("RANGE", "1_000"))
 
-partitions = System.schedulers()
-RateLimit.Atomics3.new(:sites, partitions)
-RateLimit.Counters.new(:sites_c)
+IO.puts("""
+prallel: #{parallel}
+limit: #{limit}
+scale: #{scale}
+range: #{range}
+""")
 
+# TODO: clean up ETS table before/after each scenario
 PlugAttack.Storage.Ets.start_link(:plug_attack_sites, clean_period: :timer.minutes(10))
 
 Benchee.run(
   %{
-    # "control" => fn -> :rand.uniform(1000) end,
-    "plug_attack" => fn ->
-      PlugAttack.Rule.throttle(_key = :rand.uniform(1000),
+    "plug_attack" => fn key ->
+      PlugAttack.Rule.throttle(_key = key,
         storage: {PlugAttack.Storage.Ets, :plug_attack_sites},
         limit: limit,
         period: scale
       )
     end,
-    "atomics3" => fn ->
-      RateLimit.Atomics3.hit(:sites, _key = :rand.uniform(1000), partitions, scale, limit)
-    end,
-    "hammer" => fn ->
-      Hammer.check_rate("sites:#{:rand.uniform(1000)}", scale, limit)
-    end,
-    "counters" => fn ->
-      RateLimit.Counters.hit(:sites_c, _key = :rand.uniform(1000), partitions, scale, limit)
-    end,
-    "ex_rated" => fn ->
-      ExRated.check_rate("sites:#{:rand.uniform(1000)}", scale, limit)
-    end,
-    "rate_limiter" => fn ->
-      key = "sites:#{:rand.uniform(1000)}"
+    "hammer" => fn key -> Hammer.check_rate("sites:#{key}", scale, limit) end,
+    "ex_rated" => fn key -> ExRated.check_rate("sites:#{key}", scale, limit) end,
+    "rate_limiter" => fn key ->
+      key = "sites:#{key}"
       rate_limiter = RateLimiter.get(key) || RateLimiter.new(key, scale, limit)
       RateLimiter.hit(rate_limiter)
     end
   },
+  formatters: [{Benchee.Formatters.Console, extended_statistics: true}],
+  before_each: fn _ -> :rand.uniform(range) end,
+  time: 6,
+  # fill the table with some data
+  warmup: 14,
   profile_after: profile?,
-  parallel: parallel,
-  print: [fast_warning: false]
+  parallel: parallel
 )
